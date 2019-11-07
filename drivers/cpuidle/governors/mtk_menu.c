6ae0d57a072fcce50cb77241cfd8dc4684afb7de
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2019-11-07 15:25:12 +0100

    UPSTREAM: cpuidle: Use nanoseconds as the unit of time
    Currently, the cpuidle subsystem uses microseconds as the unit of
    time which (among other things) causes the idle loop to incur some
    integer division overhead for no clear benefit.
    
    In order to allow cpuidle to measure time in nanoseconds, add two
    new fields, exit_latency_ns and target_residency_ns, to represent the
    exit latency and target residency of an idle state in nanoseconds,
    respectively, to struct cpuidle_state and initialize them with the
    help of the corresponding values in microseconds provided by drivers.
    Additionally, change cpuidle_governor_latency_req() to return the
    idle state exit latency constraint in nanoseconds.
    
    Also meeasure idle state residency (last_residency_ns in struct
    cpuidle_device and time_ns in struct cpuidle_driver) in nanoseconds
    and update the cpuidle core and governors accordingly.
    
    However, the menu governor still computes typical intervals in
    microseconds to avoid integer overflows.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Doug Smythies <dsmythies@telus.net>
    Tested-by: Doug Smythies <dsmythies@telus.net>
    
     Conflicts:
    	drivers/cpuidle/governors/ladder.c
    	drivers/cpuidle/governors/menu.c
    
    Change-Id: I19f6791d37c99dddd97978f4a2929a7ee1225ac7
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>

--- drivers/cpuidle/governors/mtk_menu.c
+++ drivers/cpuidle/governors/mtk_menu.c
@@ -561,9 +561,9 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 		}
 	}
 
-	data->last_state_idx = idx;
+	dev->last_state_idx = idx;
 
-	return data->last_state_idx;
+	return dev->last_state_idx;
 }
 
 /**
@@ -627,7 +627,7 @@ static void menu_update(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 		measured_us = 9 * MAX_INTERESTING / 10;
 	} else {
 		/* measured value */
-		measured_us = dev->last_residency;
+		measured_us = dev->last_residency_ns;
 
 		/* Deduct exit latency */
 		if (measured_us > 2 * target->exit_latency)
